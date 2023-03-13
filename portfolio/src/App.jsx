afa680ee6b91a12ba0dffdb844bfdeab969a767e
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-03-13 22:27:23 +0100

    tailwindcss added and tested

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,20 +1,20 @@
-import About from './components/About'
-import Projects from './components/Projects'
-import Skills from './components/Skills'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+// import About from './components/About'
+// import Projects from './components/Projects'
+// import Skills from './components/Skills'
+// import Contact from './components/Contact'
+// import Footer from './components/Footer'
 
 function App() {
 
   return (
     <div className="App">
-      <main>
+      {/* /* <main>
         <About />
         <Projects />
         <Skills />
         <Contact />
         <Footer />
-      </main>
+      </main> */}
     </div>
       
   )
