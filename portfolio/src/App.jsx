044b5c7867cfc6c4a32e7320d0e3f541b037ab20
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-03-13 17:37:17 +0100

    new portfolio template react

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,23 @@
+import About from './components/About'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <div className="App">
+      <main>
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+        <Footer />
+      </main>
+    </div>
+      
+  )
+}
+
+export default App
