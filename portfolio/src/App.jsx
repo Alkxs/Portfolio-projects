1bffbb73fca1904c2657f6c8e804055aaf296538
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-03-19 17:37:38 +0100

    fix mistake merging with particles branch

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -6,7 +6,6 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
 import Contact from './components/Contact'
-import Particles from './components/Particles'
 // import Footer from './components/Footer'
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
 
   return (
     <>
-      <Particles />
       <Navbar />
       <Home /> 
       <About />
