2477d7041c931972418db51317735a640815a40e
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-03-21 12:52:25 +0100

    Typewriter feature implemented

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,17 +1,24 @@
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import Typewriter from 'typewriter-effect'
 
 const Home = () => {
   return (
     <>
       <section name='home' className='bg-gray-300 dark:bg-[#0a192f] w-full h-screen'>
         {/* Container */}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-          <p className='text-violet-600'>Hi there, my name is</p>
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center items-center h-full gap-6 text-center'>
+          <p className='text-purple-600 text-2xl tracking-wide'>Hi There, I am </p>
           <h1 className='text-4xl sm:text-7xl font-bold text-[#0a192f] dark:text-white'>Alex Lauri</h1>
-          <h2 className='text-4xl sm:text-6xl font-bold text-gray-500 dark:text-gray-600'>I am a Full Stack Developer</h2>
-          <p className='text-[#8892b0] py-4 max-w-700px'>
-            Expertly designing and developing custom web applications that solve complex problems and enhance user experiences.
-          </p>
+          <h2 className='text-3xl sm:text-4xl font-light text-gray-500 dark:text-gray-400'>
+            <Typewriter
+              options={{
+                strings: ['Front-End Developer.', 'Full-stack Developer'],
+                autoStart: true,
+                loop: true,
+                skipAddStyles: true,
+              }}
+            />
+          </h2>
           <div>
             <button className='text-[#0a192f] dark:text-white group border-[#0a192f] dark:border-white border-2 px-6 py-3 my-2 flex items-center hover:bg-violet-600 dark:hover:border-violet-600 rounded-md'>
               View Work
