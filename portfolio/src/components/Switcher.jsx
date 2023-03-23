93c21422c20d497066a31a89f79cd4b88c16961f
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-03-23 21:59:18 +0100

    minor changes

--- portfolio/src/components/Switcher.jsx
+++ portfolio/src/components/Switcher.jsx
@@ -12,16 +12,13 @@ const Switcher = () => {
   }
 
   return (
-    <div 
-    // className='md:bg-gray-400 md:hover:border md:hover:border-black  md:dark:bg-gray-700 md:hover:dark:border-white hover:scale-105 md:p-2 flex items-center justify-center shadow-sm rounded-xl cursor-pointer transition duration-200'
-    >
       <DarkModeSwitch 
+      className=''
       checked={darkMode} 
       onChange={toggleDarkMode} 
       size={30} 
       moonColor='black' 
       sunColor='white' />
-    </div>
   )
 }
 export default Switcher
