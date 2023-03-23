431ad877205759625aec70f16a06fa9d5a908a6c
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-03-23 21:37:07 +0100

    minor changes

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const navBackground = scrollPosition > 800 ? 'bg-gray-300 dark:bg-[#0a192f]' : '
         {/* <img src={logo} alt='Logo Image' /> */}
       </div>
       {/* darkModeToggleIcon */}
-        <Switcher />
+      <Switcher className='fixed' />
       {/* Menu */}
       <ul className='hidden md:flex'>
         <li className='hover:text-violet-600'>
@@ -71,29 +71,35 @@ const navBackground = scrollPosition > 800 ? 'bg-gray-300 dark:bg-[#0a192f]' : '
         {showMobileMenu ? <FaTimes size={20} /> : <FaBars size={20} />}
       </div>
       {/* Mobile menu */}
-      <ul className={showMobileMenu ? 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center opacity-100' : 'hidden'}>
+      <ul
+        className={
+          showMobileMenu
+            ? 'absolute top-0 left-0 w-full h-screen bg-white-200 dark:bg-[#0a192f] flex flex-col justify-center items-center opacity-100'
+            : 'hidden'
+        }
+      >
         <li className='py-6 text-4xl '>
-          <Link onClick={handleMobileMenuToggle} to='home' smooth={true} duration={500}>
+          <Link className='text-[#0a192f] dark:text-gray-200' onClick={handleMobileMenuToggle} to='home' smooth={true} duration={500}>
             Home
           </Link>
         </li>
         <li className='py-6 text-4xl '>
-          <Link onClick={handleMobileMenuToggle} to='about' smooth={true} duration={500}>
+          <Link className='text-[#0a192f] dark:text-gray-200' onClick={handleMobileMenuToggle} to='about' smooth={true} duration={500}>
             About
           </Link>
         </li>
         <li className='py-6 text-4xl '>
-          <Link onClick={handleMobileMenuToggle} to='projects' smooth={true} duration={500}>
+          <Link className='text-[#0a192f] dark:text-gray-200' onClick={handleMobileMenuToggle} to='projects' smooth={true} duration={500}>
             Projects
           </Link>
         </li>
         <li className='py-6 text-4xl '>
-          <Link onClick={handleMobileMenuToggle} to='skills' smooth={true} duration={500}>
+          <Link className='text-[#0a192f] dark:text-gray-200' onClick={handleMobileMenuToggle} to='skills' smooth={true} duration={500}>
             Skills
           </Link>
         </li>
         <li className='py-6 text-4xl '>
-          <Link onClick={handleMobileMenuToggle} to='contact' smooth={true} duration={500}>
+          <Link className='text-[#0a192f] dark:text-gray-200' onClick={handleMobileMenuToggle} to='contact' smooth={true} duration={500}>
             Contact
           </Link>
         </li>
--- portfolio/src/components/Switcher.jsx
+++ portfolio/src/components/Switcher.jsx
@@ -16,7 +16,6 @@ const Switcher = () => {
     // className='md:bg-gray-400 md:hover:border md:hover:border-black  md:dark:bg-gray-700 md:hover:dark:border-white hover:scale-105 md:p-2 flex items-center justify-center shadow-sm rounded-xl cursor-pointer transition duration-200'
     >
       <DarkModeSwitch 
-      className=''
       checked={darkMode} 
       onChange={toggleDarkMode} 
       size={30} 
